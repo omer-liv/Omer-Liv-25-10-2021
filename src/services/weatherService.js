0f1357f82e4a067994b3caf289e848a9bc3c28bf
Omer Liv <omerleave@gmail.com>
2021-10-30 23:43:50 +0300

    https

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -18,7 +18,7 @@ function getWeatherNow(name) {
       return cities[0].Key
     })
     .then(res => {
-      const url = `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${res}?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&details=true&metric=true`
+      const url = `https://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${res}?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&details=true&metric=true`
       return axios.get(url)
         .then(res => {
           storageService.saveToStorage(HOME_KEY, res.data);
@@ -30,7 +30,7 @@ function getWeatherNow(name) {
 
 function fiveDayForecast(city) {
   const key = city ? city.Key : '215854' 
-  const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&details=true&metric=true`
+  const url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&details=true&metric=true`
   return axios.get(url)
     .then(res => {
       storageService.saveToStorage(FIVE_KEY, res.data);
@@ -40,14 +40,14 @@ function fiveDayForecast(city) {
 
 function getCityCode(name) {
   const queryName = typeof name === 'string' ? name : name.EnglishName
-  const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&q=${queryName}&language=en-us&details=true&offset=1`
+  const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&q=${queryName}&language=en-us&details=true&offset=1`
   return axios.get(url)
     .then(res => res.data)
 
 }
 
 function getCityCodeByPos(lat, lang) {
-  const url = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&q=${lat}%2C${lang}&details=true`
+  const url = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=q2991AeoNGqDqjczrkRdqwAllJ0tLVfs&q=${lat}%2C${lang}&details=true`
   return axios.get(url)
     .then(res => res.data)
 
